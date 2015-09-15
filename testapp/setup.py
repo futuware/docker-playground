bc586e41d6ee6e3a5f00291fd1ca510b572ae908
Andrew Zhu <dir01@dir01.org>
2015-09-16 00:16:24 +0300

    Added ipdb to app deps

--- testapp/setup.py
+++ testapp/setup.py
@@ -2,12 +2,13 @@
 from setuptools import setup
 
 setup(
-    name="testapp",
-    version="0.1",
-    description="Example application to play around with docker",
+    name='testapp',
+    version='0.1',
+    description='Example application to play around with docker',
     packages=['testapp'],
     install_requires=[
-        "pyramid==1.5.7",
-        "gunicorn==19.3.0",
+        'pyramid==1.5.7',
+        'gunicorn==19.3.0',
+        'ipdb'
     ],
 )
